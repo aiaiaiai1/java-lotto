061b6c2d415a89eaf62809f0bae5c8b5dbcf4637
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-12-09 22:43:03 +0900

    test: 1~45 범위의 중복되지 않는 6개 숫자 만들기 기능 테스트 추가

--- src/test/java/lotto/domain/RandomNumbersGeneratorTest.java
+++ src/test/java/lotto/domain/RandomNumbersGeneratorTest.java
@@ -1,2 +1,42 @@
-package lotto.domain;public class RandomNumbersGeneratorTest {
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RandomNumbersGeneratorTest {
+    static RandomNumbersGenerator randomNumbersGenerator = new RandomNumbersGenerator();
+    static List<Integer> numbers;
+
+    @DisplayName("6자리 중복없는 1~45의 랜덤번호 생성하기")
+    @BeforeAll
+    static void generateNumbers() {
+        numbers = randomNumbersGenerator.generateUniqueNumbers();
+    }
+
+    @DisplayName("1~45범위 인지 확인")
+    @Test
+    void isInRange() {
+        boolean result = numbers.stream().allMatch(number -> number >= 1 && number < 45);
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("6자리 인지 확인")
+    @Test
+    void isCountSix() {
+        int result = numbers.size();
+        assertThat(result).isEqualTo(6);
+    }
+
+    @DisplayName("중복이 아닌지 확인")
+    @Test
+    void isUnique() {
+        boolean result = numbers.stream().allMatch(number -> number >= 1 && number < 45);
+        assertThat(result).isTrue();
+    }
+
 }
