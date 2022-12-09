9eb44f9d7e8c64f1744ec9efbeef675831b25bd8
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-12-10 00:31:16 +0900

    test: 로또 구매하기 기능 테스트 추가

---
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class LottoMachineTest {
+    LottoMachine lottoMachine = new LottoMachine() {
+        protected Lotto generateRandomLotto() {
+            return new Lotto(List.of(2, 1, 3, 4, 5, 6));
+        }
+    };
+
+    @DisplayName("로또 구입금액이 0인 경우와 1000원으로 나누어 떨어지지 않는 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1500})
+    void purchaseLottoByInValidMoney(int money) {
+        Money purchasingMoney = new Money(money);
+        assertThatThrownBy(
+                () -> lottoMachine.purchaseLotto(purchasingMoney)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 구매하기")
+    @Test
+    void purchaseLotto() {
+        lottoMachine.purchaseLotto(new Money(2000));
+        List<Lotto> result = lottoMachine.getLottoTable();
+
+        List<Lotto> expected = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+
+        assertThat(result).usingRecursiveComparison().isEqualTo(expected);
+
+    }
+
+}
