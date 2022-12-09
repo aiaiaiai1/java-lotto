146c94c7b2d4bb22e982e34010b9907a41d07933
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-12-10 00:31:35 +0900

    test: Money 클래스 테스트 추가

---
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -0,0 +1,37 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class MoneyTest {
+    @DisplayName("돈 객체 만들기")
+    @Test
+    void createMoney() {
+        Money money = new Money(5000);
+        int result = money.getMoney();
+        assertThat(result).isEqualTo(5000);
+    }
+
+    @DisplayName("돈이 나누어 떨어지는 경우 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"1000,true", "2000,false"})
+    void checkDivisibility(int divisor, boolean isDivisible) {
+        Money money = new Money(5000);
+        boolean result = money.isDivisible(divisor);
+        assertThat(result).isEqualTo(isDivisible);
+    }
+
+    @DisplayName("돈이 없는 경우 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"0,true", "2000,false"})
+    void checkZero(int money, boolean isZero) {
+        boolean result = new Money(money).isZero();
+        assertThat(result).isEqualTo(isZero);
+    }
+
+}
