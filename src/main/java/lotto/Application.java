de02834f87438ee6e402f44f306c5cc78e9ba4f0
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-12-15 19:32:09 +0900

    feat: 프로그램 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.Controller.GameContorller;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameContorller gameContorller = new GameContorller();
+        gameContorller.run();
     }
 }
