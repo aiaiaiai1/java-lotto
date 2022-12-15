ad895015754e6e3c5ace4b726b4ca4c3e86ec13d
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-12-15 19:26:37 +0900

    feat: Money클래스 validate 메서드 추가
    - 돈이 음수이면 예외를 발생시키는 메서드

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -4,11 +4,18 @@ public class Money {
     private final int money;
 
     public Money(int money) {
+        validate(money);
         this.money = money;
     }
 
     public int getMoney() {
         return money;
+    }
+
+    private void validate(int money) {
+        if (money < 0){
+            throw new IllegalArgumentException("[ERROR] 잘못된 금액입니다.");
+        }
     }
 
     public boolean isDivisible(int divisor) {
